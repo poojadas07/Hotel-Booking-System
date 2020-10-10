2aa91dad78ac940edbc4e0bd6998b551d155ce44
poojadas07 <poojadas04kv@gmail.com>
2020-10-11 02:33:42 +0530

    Styles added

--- registration.php
+++ registration.php
@@ -4,7 +4,11 @@
 	<title>SignIn</title>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Cinzel Decorative' rel='stylesheet'>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
@@ -13,16 +17,45 @@
 		<div class="row">
 			<div class="col-sm-6 left">
 				<h1>Welcome Back</h1>
+				<h3>Sign In <br> to Continue Access</h3>
 			</div>
-			<div class="col-sm-6 right">
-				<h1>Hello World!</h1>
+			<div class="col-sm-6 right ">
+				<h1 class="pb-3 pt-5 pl-5 pr-5">Sign In</h1>
+
+				<div class="row pl-5 pr-5">
+				    <div class="col-12">
+				      <div class="md-form mt-0">
+				      	<input type="text" id="form1" class="form-control" width="50%">
+						<label for="form1">Username / Email</label>
+				      </div>
+				    </div>
+				    <div class="col-12">
+				      <div class="md-form mt-0">
+				        <input type="text" id="form1" class="form-control">
+						<label for="form1">Password</label>
+				      </div>
+				    </div>
+				</div>
+				<div class="row ml-4 mr-4">
+					<a class="btn btn-primary btn-lg active btn-block" role="button" aria-pressed="true">Continue <b> &gt; &gt;</b></a>
+				</div>
+				<br>
+				<p style="text-align: center;">or Connect with Social Media</p>
+
+				<div class="row ml-4 mr-4">
+					<button type="button" class="btn blue-gradient btn-fb btn-md btn-block text-left mb-3"><i class="fab fa-facebook-f pr-1"></i> Sign In with Facebook</button>
+					<button type="button" class="btn aqua-gradient btn-tw btn-md btn-block text-left mb-3"><i class="fab fa-twitter pr-1"></i> Sign In with Twitter</button>
+					<button type="button" class="btn purple-gradient btn-gplus btn-md btn-block text-left mb-3"><i class="fab fa-google-plus-g pr-1"></i> Sign In with Google +</button>
+				</div>
+
 			</div>
 		</div>
 	</div>
 
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
 
 
 </body>
