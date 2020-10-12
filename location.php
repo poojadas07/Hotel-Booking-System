12ed4e245bb697920d6a143da12236892c114db7
poojadas07 <poojadas04kv@gmail.com>
2020-10-12 08:48:11 +0530

    Location Page added

---
+++ location.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Locations</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ 
+    <link href='https://fonts.googleapis.com/css?family=Cinzel Decorative' rel='stylesheet'>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+
+	<div class="container">
+		<div class="row">
+			<h1> Choose Your Location  </h1>
+
+			<div class="jumbotron text-center hoverable p-4">
+				<div class="row">
+					<div class="col-md-4 offset-md-1 mx-3 my-3">
+						<div class="view overlay">
+	        				<img src="images/jodhpur.jpg" class="img-fluid" alt="Jodhpur">
+	        				<a>
+	        					<div class="mask rgba-white-slight"></div>
+	        				</a>
+	      				</div>
+					</div>
+	    			
+	    			<div class="col-md-7 text-md-left ml-3 mt-3">
+						<a href="#!" class="green-text">
+	        				<h6 class="h6 pb-1"><i class="fas fa-map-marker-alt pr-1"></i> Destination </h6>
+	      				</a>
+						<h4 class="h4 mb-4">This is title of the news</h4>
+						<p class="font-weight-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore agna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.</p>
+					    <p class="font-weight-normal">by <a><strong>Carine Fox</strong></a>, 19/08/2016</p>
+
+	    			</div>
+	  			</div>
+	  			<div class="hide">
+					<a href="#!" class="red-text d-flex flex-row-reverse p-2">
+						<h5 class="waves-effect waves-light"><b>Read more</b><i class="fas fa-angle-double-right ml-2"></i></h5>
+					</a>
+				</div>
+	  			
+			</div>
+
+			<div class="jumbotron text-center hoverable p-4">
+				<div class="row">
+	    			
+	    			<div class="col-md-7 text-md-left ml-3 mt-3">
+						<a href="#!" class="green-text">
+	        				<h6 class="h6 pb-1"><i class="fas fa-map-marker-alt pr-1"></i> Destination </h6>
+	      				</a>
+						<h4 class="h4 mb-4">This is title of the news</h4>
+						<p class="font-weight-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore agna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.</p>
+					    <p class="font-weight-normal">by <a><strong>Carine Fox</strong></a>, 19/08/2016</p>
+
+	    			</div>
+	    			<div class="col-md-4 offset-md-1 mx-3 my-3">
+						<div class="view overlay">
+	        				<img src="images/jaipur.jpg" class="img-fluid" alt="Jaipur" height="50px">
+	        				<a>
+	        					<div class="mask rgba-white-slight"></div>
+	        				</a>
+	      				</div>
+					</div>
+	  			</div>
+	  			<div class="hide">
+					<a href="#!" class="red-text d-flex flex-row-reverse p-2">
+						<h5 class="waves-effect waves-light"><b>Read more</b><i class="fas fa-angle-double-right ml-2"></i></h5>
+					</a>
+				</div>
+			</div>
+
+			<div class="jumbotron text-center hoverable p-4">
+				<div class="row">
+	    			
+	    			<div class="col-md-4 offset-md-1 mx-3 my-3">
+						<div class="view overlay">
+	        				<img src="images/udaipur.jpg" class="img-fluid" alt="Udaipur">
+	        				<a>
+	        					<div class="mask rgba-white-slight"></div>
+	        				</a>
+	      				</div>
+					</div>
+
+	    			<div class="col-md-7 text-md-left ml-3 mt-3">
+						<a href="#!" class="green-text">
+	        				<h6 class="h6 pb-1"><i class="fas fa-map-marker-alt pr-1"></i> Destination </h6>
+	      				</a>
+						<h4 class="h4 mb-4">This is title of the news</h4>
+						<p class="font-weight-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore agna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.</p>
+					    <p class="font-weight-normal">by <a><strong>Carine Fox</strong></a>, 19/08/2016</p>
+
+	    			</div>
+	    			
+	  			</div>
+	  			<div class="hide">
+					<a href="#!" class="red-text d-flex flex-row-reverse p-2">
+						<h5 class="waves-effect waves-light"><b>Read more</b><i class="fas fa-angle-double-right ml-2"></i></h5>
+					</a>
+				</div>
+			</div>
+
+			<div class="jumbotron text-center hoverable p-4">
+				<div class="row">
+	    			
+	    			<div class="col-md-7 text-md-left ml-3 mt-3">
+						<a href="#!" class="green-text">
+	        				<h6 class="h6 pb-1"><i class="fas fa-map-marker-alt pr-1"></i> Destination </h6>
+	      				</a>
+						<h4 class="h4 mb-4">This is title of the news</h4>
+						<p class="font-weight-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore agna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.</p>
+					    <p class="font-weight-normal">by <a><strong>Carine Fox</strong></a>, 19/08/2016</p>
+
+	    			</div>
+	    			<div class="col-md-4 offset-md-1 mx-3 my-3">
+						<div class="view overlay">
+	        				<img src="images/Jaisalmer.jpg" class="img-fluid" alt="Jaisalmer">
+	        				<a>
+	        					<div class="mask rgba-white-slight"></div>
+	        				</a>
+	      				</div>
+					</div>
+	  			</div>
+	  			<div class="hide">
+					<a href="#!" class="red-text d-flex flex-row-reverse p-2">
+						<h5 class="waves-effect waves-light"><b>Read more</b><i class="fas fa-angle-double-right ml-2"></i></h5>
+					</a>
+				</div>
+			</div>
+
+			<div class="jumbotron text-center hoverable p-4">
+				<div class="row">
+	    			
+	    			<div class="col-md-4 offset-md-1 mx-3 my-3">
+						<div class="view overlay">
+	        				<img src="images/mount_abu.jpg" class="img-fluid" alt="Mount Abu">
+	        				<a>
+	        					<div class="mask rgba-white-slight"></div>
+	        				</a>
+	      				</div>
+					</div>
+	    			<div class="col-md-7 text-md-left ml-3 mt-3">
+						<a href="#!" class="green-text">
+	        				<h6 class="h6 pb-1"><i class="fas fa-map-marker-alt pr-1"></i> Destination </h6>
+	      				</a>
+						<h4 class="h4 mb-4">This is title of the news</h4>
+						<p class="font-weight-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore agna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.</p>
+					    <p class="font-weight-normal">by <a><strong>Carine Fox</strong></a>, 19/08/2016</p>
+
+	    			</div>
+	    			
+	  			</div>
+	  			<div class="hide">
+					<a href="#!" class="red-text d-flex flex-row-reverse p-2">
+						<h5 class="waves-effect waves-light"><b>Read more</b><i class="fas fa-angle-double-right ml-2"></i></h5>
+					</a>
+				</div>
+			</div>
+
+			
+		</div>
+	</div>
+
+
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
+
+
+</body>
+</html>
