1ee5762ed2b466c916fd25e96bd9790a82cdc32e
poojadas07 <poojadas04kv@gmail.com>
2020-10-12 15:05:49 +0530

    Details Page added

--- hotel.php
+++ hotel.php
@@ -18,15 +18,15 @@
 		<div class="row">
 			<div class="col-sm-6">
 				<h1>Restaurants Near Me</h1><br>
-				<a href="#"><p class="head">Bread Basket<i class="fas fa-angle-double-right ml-2"></i></p></a>
+				<a href="#"><p class="head"> Bread Basket <i class="fas fa-angle-double-right ml-2"></i></p></a>
 				<p>Assortment of fresh baked fruit breads and muffins 5.50</p><br>
-				<a href="#"><p class="head">Honey Almond Granola with Fruits<i class="fas fa-angle-double-right ml-2"></i></p></a>
+				<a href="#"><p class="head"> Honey Almond Granola with Fruits <i class="fas fa-angle-double-right ml-2"></i></p></a>
 				<p>Natural cereal of honey toasted oats, raisins, almonds and dates 7.00</p><br>
-				<a href="#"><p class="head">Belgian Waffle<i class="fas fa-angle-double-right ml-2"></i></p></a>
+				<a href="#"><p class="head"> Belgian Waffle <i class="fas fa-angle-double-right ml-2"></i></p></a>
 				<p>Vanilla flavored batter with malted flour 7.50</p><br>
-				<a href="#"><p class="head">Scrambled eggs<i class="fas fa-angle-double-right ml-2"></i></p></a>
+				<a href="#"><p class="head"> Scrambled eggs <i class="fas fa-angle-double-right ml-2"></i></p></a>
 				<p>Scrambled eggs, roasted red pepper and garlic, with green onions 7.50</p><br>
-				<a href="#"><p class="head">Blueberry Pancakes<i class="fas fa-angle-double-right ml-2"></i></p></a>
+				<a href="#"><p class="head"> Blueberry Pancakes <i class="fas fa-angle-double-right ml-2"></i></p></a>
 				<p>With syrup, butter and lots of berries 8.50</p><br>
 			</div>
 			<div class="col-sm-6">
