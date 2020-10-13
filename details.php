a82bc59cee3fa3c9a2bd3d1df614e386a072a5f5
poojadas07 <poojadas04kv@gmail.com>
2020-10-13 10:46:33 +0530

    Styles added to details page

--- details.php
+++ details.php
@@ -1,3 +1,16 @@
+<?php
+	session_start();
+
+	if (!isset($_SESSION['username'])) {
+		header('location:login.php');
+	}
+
+	$con = mysqli_connect('localhost' , 'root' , 'root');
+
+	mysqli_select_db($con , 'tourist');
+	
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,6 +26,28 @@
 	<link rel="stylesheet" type="text/css" href="hotel.css">
 </head>
 <body>
+
+	<div class="card">
+		<img src="images/left1.jpg" alt="photo" style="width:100%">
+		<div class="dropdown">
+
+			<a class="dropdown" data-toggle="dropdown">
+				<p style="font-weight: 900; font-family: 'Cinzel Decorative'">
+					<?php echo $_SESSION['username']; ?>
+					<i class="fas fa-angle-down ml-2"></i>
+				</p>
+			</a>
+
+			<div class="dropdown-menu">
+			    <a class="dropdown-item" href="logout.php"> Logout </a>
+			</div>
+
+		</div>
+		
+	</div>
+
+	
+	
 
 
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
